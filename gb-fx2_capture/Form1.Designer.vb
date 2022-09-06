37a4426daa7431b262a50b14f8f7668fb22021fb
Eocore Kuneho <schindlerlorant@msn.com>
2022-09-06 12:20:58 +0200

    Add project files.

---
+++ gb-fx2_capture/Form1.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pbx_Screen = New System.Windows.Forms.PictureBox()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.tstbx_Port = New System.Windows.Forms.ToolStripTextBox()
+        Me.ToolStripLabel2 = New System.Windows.Forms.ToolStripLabel()
+        Me.tstbx_Baud = New System.Windows.Forms.ToolStripTextBox()
+        Me.tsbtn_Connect = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.tsbtn_Disconnect = New System.Windows.Forms.ToolStripButton()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.tsslbl_Status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tsslbl_ByteStat = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tsslbl_DATA0 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tsslbl_xypos = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.btn_Color0 = New System.Windows.Forms.Button()
+        Me.btn_Color1 = New System.Windows.Forms.Button()
+        Me.btn_Color2 = New System.Windows.Forms.Button()
+        Me.btn_Color3 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        CType(Me.pbx_Screen, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ToolStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pbx_Screen
+        '
+        Me.pbx_Screen.BackColor = System.Drawing.Color.Black
+        Me.pbx_Screen.Location = New System.Drawing.Point(12, 57)
+        Me.pbx_Screen.Name = "pbx_Screen"
+        Me.pbx_Screen.Size = New System.Drawing.Size(320, 280)
+        Me.pbx_Screen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbx_Screen.TabIndex = 0
+        Me.pbx_Screen.TabStop = False
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1, Me.tstbx_Port, Me.ToolStripLabel2, Me.tstbx_Baud, Me.tsbtn_Connect, Me.ToolStripSeparator1, Me.tsbtn_Disconnect})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+        Me.ToolStrip1.Size = New System.Drawing.Size(343, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(32, 22)
+        Me.ToolStripLabel1.Text = "Port:"
+        '
+        'tstbx_Port
+        '
+        Me.tstbx_Port.Name = "tstbx_Port"
+        Me.tstbx_Port.Size = New System.Drawing.Size(100, 25)
+        Me.tstbx_Port.Text = "COM9"
+        '
+        'ToolStripLabel2
+        '
+        Me.ToolStripLabel2.Name = "ToolStripLabel2"
+        Me.ToolStripLabel2.Size = New System.Drawing.Size(42, 22)
+        Me.ToolStripLabel2.Text = "Speed:"
+        '
+        'tstbx_Baud
+        '
+        Me.tstbx_Baud.Name = "tstbx_Baud"
+        Me.tstbx_Baud.Size = New System.Drawing.Size(100, 25)
+        Me.tstbx_Baud.Text = "115200"
+        '
+        'tsbtn_Connect
+        '
+        Me.tsbtn_Connect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbtn_Connect.Image = CType(resources.GetObject("tsbtn_Connect.Image"), System.Drawing.Image)
+        Me.tsbtn_Connect.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbtn_Connect.Name = "tsbtn_Connect"
+        Me.tsbtn_Connect.Size = New System.Drawing.Size(56, 19)
+        Me.tsbtn_Connect.Text = "Connect"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'tsbtn_Disconnect
+        '
+        Me.tsbtn_Disconnect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbtn_Disconnect.Image = CType(resources.GetObject("tsbtn_Disconnect.Image"), System.Drawing.Image)
+        Me.tsbtn_Disconnect.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbtn_Disconnect.Name = "tsbtn_Disconnect"
+        Me.tsbtn_Disconnect.Size = New System.Drawing.Size(70, 19)
+        Me.tsbtn_Disconnect.Text = "Disconnect"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsslbl_Status, Me.tsslbl_ByteStat, Me.tsslbl_DATA0, Me.tsslbl_xypos})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 343)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(343, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'tsslbl_Status
+        '
+        Me.tsslbl_Status.Name = "tsslbl_Status"
+        Me.tsslbl_Status.Size = New System.Drawing.Size(43, 17)
+        Me.tsslbl_Status.Text = "Offline"
+        '
+        'tsslbl_ByteStat
+        '
+        Me.tsslbl_ByteStat.Name = "tsslbl_ByteStat"
+        Me.tsslbl_ByteStat.Size = New System.Drawing.Size(22, 17)
+        Me.tsslbl_ByteStat.Text = "---"
+        '
+        'tsslbl_DATA0
+        '
+        Me.tsslbl_DATA0.Name = "tsslbl_DATA0"
+        Me.tsslbl_DATA0.Size = New System.Drawing.Size(12, 17)
+        Me.tsslbl_DATA0.Text = "-"
+        '
+        'tsslbl_xypos
+        '
+        Me.tsslbl_xypos.Name = "tsslbl_xypos"
+        Me.tsslbl_xypos.Size = New System.Drawing.Size(40, 17)
+        Me.tsslbl_xypos.Text = "X:- Y:-"
+        '
+        'btn_Color0
+        '
+        Me.btn_Color0.BackColor = System.Drawing.Color.Black
+        Me.btn_Color0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Color0.Location = New System.Drawing.Point(12, 28)
+        Me.btn_Color0.Name = "btn_Color0"
+        Me.btn_Color0.Size = New System.Drawing.Size(25, 23)
+        Me.btn_Color0.TabIndex = 3
+        Me.btn_Color0.UseVisualStyleBackColor = False
+        '
+        'btn_Color1
+        '
+        Me.btn_Color1.BackColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer))
+        Me.btn_Color1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Color1.Location = New System.Drawing.Point(36, 28)
+        Me.btn_Color1.Name = "btn_Color1"
+        Me.btn_Color1.Size = New System.Drawing.Size(25, 23)
+        Me.btn_Color1.TabIndex = 4
+        Me.btn_Color1.UseVisualStyleBackColor = False
+        '
+        'btn_Color2
+        '
+        Me.btn_Color2.BackColor = System.Drawing.Color.FromArgb(CType(CType(170, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(170, Byte), Integer))
+        Me.btn_Color2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Color2.Location = New System.Drawing.Point(60, 28)
+        Me.btn_Color2.Name = "btn_Color2"
+        Me.btn_Color2.Size = New System.Drawing.Size(25, 23)
+        Me.btn_Color2.TabIndex = 5
+        Me.btn_Color2.UseVisualStyleBackColor = False
+        '
+        'btn_Color3
+        '
+        Me.btn_Color3.BackColor = System.Drawing.Color.White
+        Me.btn_Color3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Color3.Location = New System.Drawing.Point(84, 28)
+        Me.btn_Color3.Name = "btn_Color3"
+        Me.btn_Color3.Size = New System.Drawing.Size(25, 23)
+        Me.btn_Color3.TabIndex = 6
+        Me.btn_Color3.UseVisualStyleBackColor = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Greyscale", "Game Boy"})
+        Me.ComboBox1.Location = New System.Drawing.Point(115, 28)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(216, 23)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(343, 365)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.btn_Color3)
+        Me.Controls.Add(Me.btn_Color2)
+        Me.Controls.Add(Me.btn_Color1)
+        Me.Controls.Add(Me.btn_Color0)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.pbx_Screen)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "gb-fx2 Capture"
+        CType(Me.pbx_Screen, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pbx_Screen As PictureBox
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents tstbx_Port As ToolStripTextBox
+    Friend WithEvents ToolStripLabel2 As ToolStripLabel
+    Friend WithEvents tstbx_Baud As ToolStripTextBox
+    Friend WithEvents tsbtn_Connect As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents tsbtn_Disconnect As ToolStripButton
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents tsslbl_Status As ToolStripStatusLabel
+    Friend WithEvents tsslbl_ByteStat As ToolStripStatusLabel
+    Friend WithEvents btn_Color0 As Button
+    Friend WithEvents btn_Color1 As Button
+    Friend WithEvents btn_Color2 As Button
+    Friend WithEvents btn_Color3 As Button
+    Friend WithEvents tsslbl_DATA0 As ToolStripStatusLabel
+    Friend WithEvents tsslbl_xypos As ToolStripStatusLabel
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ gb-fx2_capture/Form1.vb
@@ -0,0 +1,184 @@
+﻿Imports System.IO.Ports
+Public Class Form1
+    Public Event SerialPortDataToProcess(sender As Object, e As SerialPortDataToProcessEventArgs)
+
+    Private S_HEIGHT As Integer = 144
+    Private S_WIDTH As Integer = 160
+    Private V_PACKSIZE As Integer = 4
+    Private S_SIZE As Integer = (S_WIDTH * S_HEIGHT) / V_PACKSIZE
+    Private COLORS(4) As Color
+    Private screen_buffer(S_SIZE) As Byte
+
+    Private serial_port As SerialPort
+
+    Private color_schemes As List(Of Color())
+
+    Private Sub Close_SerialPort()
+        Try
+            RemoveHandler serial_port.DataReceived, AddressOf serial_port_DataReceived
+            If serial_port.IsOpen Then
+                serial_port.DiscardInBuffer()
+                serial_port.DiscardOutBuffer()
+                serial_port.Close()
+            End If
+            If serial_port.IsOpen Then
+                tsslbl_Status.Text = "Online"
+                tstbx_Port.Enabled = False
+                tstbx_Baud.Enabled = False
+                tsbtn_Connect.Enabled = False
+                tsbtn_Disconnect.Enabled = True
+            Else
+                tsslbl_Status.Text = "Offline"
+                tstbx_Port.Enabled = True
+                tstbx_Baud.Enabled = True
+                tsbtn_Connect.Enabled = True
+                tsbtn_Disconnect.Enabled = False
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub tsbtn_Connect_Click(sender As Object, e As EventArgs) Handles tsbtn_Connect.Click
+        Try
+            serial_port = New SerialPort()
+            AddHandler serial_port.DataReceived, AddressOf serial_port_DataReceived
+            serial_port.PortName = tstbx_Port.Text
+            serial_port.BaudRate = tstbx_Baud.Text
+            serial_port.DtrEnable = True
+            serial_port.Open()
+            'bgw_DataReader.RunWorkerAsync()
+        Catch ex As Exception
+            MsgBox("Couldn't open serial port." & vbCrLf & ex.Message, MsgBoxStyle.Critical)
+            If serial_port.IsOpen Then
+                serial_port.DiscardInBuffer()
+                serial_port.DiscardOutBuffer()
+                Close_SerialPort()
+            End If
+            Return
+        End Try
+        If serial_port.IsOpen Then
+            tsslbl_Status.Text = "Online"
+            tstbx_Port.Enabled = False
+            tstbx_Baud.Enabled = False
+            tsbtn_Connect.Enabled = False
+            tsbtn_Disconnect.Enabled = True
+        Else
+            tsslbl_Status.Text = "Offline"
+            tstbx_Port.Enabled = True
+            tstbx_Baud.Enabled = True
+            tsbtn_Connect.Enabled = True
+            tsbtn_Disconnect.Enabled = False
+        End If
+    End Sub
+
+    Private Sub tsbtn_Disconnect_Click(sender As Object, e As EventArgs) Handles tsbtn_Disconnect.Click
+        Close_SerialPort()
+    End Sub
+
+
+    Private Sub serial_port_DataReceived(sender As Object, e As SerialDataReceivedEventArgs)
+        Do While serial_port.BytesToRead > 0
+            Try
+                Dim data_in As Byte = serial_port.ReadByte()
+                'Me.Invoke(New EventHandler(AddressOf Process_SerialData))
+                RaiseEvent SerialPortDataToProcess(serial_port, New SerialPortDataToProcessEventArgs(data_in))
+            Catch ex As Exception
+                'Debug.Print("DataReceived: " & ex.Message)
+            End Try
+        Loop
+    End Sub
+
+    Dim x As Integer = 0
+    Dim y As Integer = 0
+    Dim img_chunk As Integer = 0
+    Dim image = New Bitmap(S_WIDTH, S_HEIGHT)
+    Private Sub Process_SerialData(ByVal sender As Object, ByVal e As SerialPortDataToProcessEventArgs) Handles Me.SerialPortDataToProcess
+        For i As Integer = V_PACKSIZE - 1 To 0 Step -1
+            Dim pixeldata As Byte
+            pixeldata = e.Data >> (i * 2) And &H3
+            'tsslbl_DATA0.Text = pixeldata
+            image.SetPixel(x + Math.Abs(((V_PACKSIZE - 1) - i)), y, COLORS(pixeldata))
+        Next
+        If x < S_WIDTH - V_PACKSIZE - 1 Then
+            x += V_PACKSIZE
+        Else
+            x = 0
+            If y < S_HEIGHT - 1 Then
+                y += 1
+            Else
+                y = 0
+                Process_Image(image)
+            End If
+        End If
+    End Sub
+
+    Private Sub Process_Image(ByRef image As Image)
+        Try
+            pbx_Screen.Image = image
+        Catch ex As Exception
+            tsslbl_xypos.Text = ex.Message
+            'Debug.Print(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        color_schemes = New List(Of Color())
+        Dim gbcolors(4) As Color
+        gbcolors(0) = Color.FromArgb(255, 0, 0, 0)
+        gbcolors(1) = Color.FromArgb(255, 85, 85, 85)
+        gbcolors(2) = Color.FromArgb(255, 170, 170, 170)
+        gbcolors(3) = Color.FromArgb(255, 255, 255, 255)
+        Dim gbcolors2(4) As Color
+        gbcolors2(0) = Color.FromArgb(255, 15, 56, 15)
+        gbcolors2(1) = Color.FromArgb(255, 48, 98, 48)
+        gbcolors2(2) = Color.FromArgb(255, 139, 172, 15)
+        gbcolors2(3) = Color.FromArgb(255, 155, 188, 15)
+
+        color_schemes.Add(gbcolors)
+        color_schemes.Add(gbcolors2)
+        ComboBox1.SelectedIndex = 0
+        tsbtn_Disconnect.Enabled = False
+    End Sub
+
+    Private Sub UpdateColor(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Color0.Click, btn_Color1.Click, btn_Color2.Click, btn_Color3.Click
+        If sender IsNot Nothing Then
+            Dim opencolordialog As New ColorDialog
+            If opencolordialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+                sender.BackColor = opencolordialog.Color
+            Else
+                Return
+            End If
+        End If
+        COLORS(0) = btn_Color0.BackColor
+        COLORS(1) = btn_Color1.BackColor
+        COLORS(2) = btn_Color2.BackColor
+        COLORS(3) = btn_Color3.BackColor
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Close_SerialPort()
+        e.Cancel = False
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        btn_Color0.BackColor = color_schemes(ComboBox1.SelectedIndex)(0)
+        btn_Color1.BackColor = color_schemes(ComboBox1.SelectedIndex)(1)
+        btn_Color2.BackColor = color_schemes(ComboBox1.SelectedIndex)(2)
+        btn_Color3.BackColor = color_schemes(ComboBox1.SelectedIndex)(3)
+        UpdateColor(Nothing, Nothing)
+    End Sub
+End Class
+
+Public Class SerialPortDataToProcessEventArgs
+    Inherits EventArgs
+
+    Public Property Data As Byte
+
+    Public Sub New(wData As Byte)
+        MyBase.New()
+        Me.Data = wData
+    End Sub
+End Class
